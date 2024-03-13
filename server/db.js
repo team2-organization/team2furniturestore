ad6a50dddfd99a084ac72b5a54b199a42e7bc74c
chiemelaumeh <chiemelaumeh1@gmail.com>
2024-03-13 17:51:58 -0500

    new

--- server/db.js
+++ server/db.js
@@ -5,14 +5,15 @@ dotenv.config();
 
 // Connect To Database
 const pool = mysql.createPool({
-    user: "7vi7u0g3pevg6flhfa0t",
-    host: "aws.connect.psdb.cloud",
-    password: "pscale_pw_x4NuXdb9Zg2geYS6usNK01WYYrZL32gnRoW4dienKTh",
-    database: "cosc-3380-team-project-db",
-    ssl: "mysql://7vi7u0g3pevg6flhfa0t:pscale_pw_x4NuXdb9Zg2geYS6usNK01WYYrZL32gnRoW4dienKTh@us-east.connect.psdb.cloud/cosc-3380-team-project-db" ?{ rejectUnauthorized: false } : false
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    ssl: process.env.DATABASE_URL ?{ rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
+
 
 // const DATABASE_URL = process.env.DATABASE_URL;
 // export const pool = mysql.createPool(DATABASE_URL).promise();
